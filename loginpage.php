0fd414f9f62dfc38e6891b84349e2d4ee89c2426
Aleandero <skarsnik.grimgor1@gmail.com>
2022-11-23 15:10:08 +0100

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -16,7 +16,18 @@
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="assets/css/mbr-additional.css" type="text/css">
   
-  
+  <style>
+    form {border: 3px solid #f1f1f1;}
+
+    input[type=text], input[type=password] {
+        width: 100%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+    }
+</style>
   
   
 </head>
