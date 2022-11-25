edcd6105173aed3b44254dbfd94b59f56c159c77
Aleandero <skarsnik.grimgor1@gmail.com>
2022-11-25 13:50:04 +0100

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -111,6 +111,7 @@
 
 <?php
     $link = mysql_connect('sql7.freesqldatabase.com:3306', 'sql7580097', 'RWuYRdbM7h');
+    $db_name = "sql7580097";  
     if (!$link) {
         die('Could not connect: ' . mysql_error());
     }
