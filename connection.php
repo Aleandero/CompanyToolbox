fe62867f39a583fb31c40e38bf957251eecf9f98
Aleandero <skarsnik.grimgor1@gmail.com>
2022-11-25 14:10:40 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,4 @@
+<html>
 <?php
     $link = mysql_connect('sql7.freesqldatabase.com:3306', 'sql7580097', 'RWuYRdbM7h');
     $db_name = "sql7580097";  
@@ -7,3 +8,4 @@
     echo 'Connected successfully';
     mysql_close($link);
 ?>
+</html>
