f0f79adc35f4126c26c6ee0dbd5e9f3c7e457d16
Aleandero <skarsnik.grimgor1@gmail.com>
2022-11-25 16:05:39 +0100

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,17 @@
+<?php
+include "config.php";
+
+// Check user login or not
+if(!isset($_SESSION['uname'])){
+    header('Location: index.php');
+}
+
+// logout
+if(isset($_POST['but_logout'])){
+    session_destroy();
+    header('Location: index.php');
+}
+?>
 <!DOCTYPE html>
 <html  >
 <head>
@@ -9,7 +23,7 @@
 </head>
 <body>
   <?php
-  echo Hello!!
+  echo ("Hello!!!");
   ?>
 </body>
 
