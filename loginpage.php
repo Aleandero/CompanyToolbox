b0c3dc35c454061b5dbf6ff0a95ae9925b7d7622
Aleandero <118822117+Aleandero@users.noreply.github.com>
2022-12-02 14:41:14 +0100

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -36,14 +36,25 @@ if(isset($_POST['but_submit'])){
   <meta name="description" content="Company">
   
   
-  <title>Home</title>
+  <title>Login</title>
   <link rel="stylesheet" href="assets/css/icons/mobirise2.css">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
   <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
   <link rel="stylesheet" href="assets/css/style.css">
   <link rel="stylesheet" href="assets/css/mbr-additional.css" type="text/css">
-  
+  <link rel="stylesheet" href="assets/loged/stylehome.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
   <style>
 
 /* Login */
@@ -89,31 +100,19 @@ if(isset($_POST['but_submit'])){
 </head>
 <body>
   
-  <section class="menu cid-s48OLK6784">
-    
-    <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
-        <div class="container">
-            <div class="navbar-brand">
-                <span class="navbar-logo">
-                    <a href="index.php">
-                        <img src="assets/images/company-1-121x121.png" style="height: 3.8rem;">
-                    </a>
-                </span>
-                <span class="navbar-caption-wrap"><a class="navbar-caption text-black display-4" href="index.php">Company Toolbox</a></span>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="index.php"><span class="mobi-mbri mobi-mbri-home mbr-iconfont mbr-iconfont-btn"></span>Home</a></li>
-            <li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="page1.php"><span class="mobi-mbri mobi-mbri-add-submenu mbr-iconfont mbr-iconfont-btn"></span>Schedule</a></li>
-            <li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="page2.php"><span class="mobi-mbri mobi-mbri-italic mbr-iconfont mbr-iconfont-btn"></span>New Information</a></li>
-            <li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="page3.php"><span class="mbri-mobile mbr-iconfont mbr-iconfont-btn"></span>Contact</a></li>
-            <li class="nav-item"><a class="nav-link link text-black text-primary display-4" href="loginpage.php"><span class="mbri-mobile mbr-iconfont mbr-iconfont-btn"></span>Log in</a></li></ul>
-               
-                
-            </div>
-        </div>
-    </nav>
-
-</section>
+   <div class="topnav">
+  <a href="home.php" class="active">Company Toolbox</a>
+  <div id="myLinks">
+    <a href="Index.php">Main Page</a>
+    <a href="page1.php">Schedule</a>
+	<a href="page2.php">News</a>
+	<a href="page3.php">Contact</a>
+    <a href="loginpage.php">Log in</a>
+  </div>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
 <section class="form5 cid-tn4X0jmbyb mbr-Fullscreen " >
 </section>
 <section class="form5 cid-tn4X0jmbyb mbr-Fullscreen " >
