477f2632385dcc8f003f6523032c58993a50a81f
Aleandero <118822117+Aleandero@users.noreply.github.com>
2022-12-02 14:40:21 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -21,7 +21,7 @@ if(isset($_POST['but_logout'])){
   <link rel="shortcut icon" href="assets/images/company-1-121x121.png" type="image/x-icon">
   <meta name="description" content="Company">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <title>Home</title>
+  <title>Contact</title>
  <style> 
  <?php
 include "assets/loged/stylehome.css"
@@ -31,10 +31,10 @@ include "assets/loged/stylehome.css"
 <body>
 
 <div class="topnav">
-  <a href="Home.php" class="active">Company Toolbox</a>
+  <a href="home.php" class="active">Koko Company</a>
   <div id="myLinks">
     <a href="news.php">News</a>
-    <a href="schedule.php">Contact</a>
+    <a href="schedule.php">Schedule</a>
 	<a href="blog.php">Blog</a>
 	<a href="contact.php">Contact</a>
     <a href="logout.php">Log out</a>
@@ -43,15 +43,34 @@ include "assets/loged/stylehome.css"
     <i class="fa fa-bars"></i>
   </a>
 </div>
-Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>Home!!!<br>
+	<form id="contactform" action="https://formsubmit.io/send/kamil.alderio@gmail.com" method="POST">
+	<label for="User">Your user Name*</label>
+	<input
+		id="Employee"
+		name="Employee"
+		type="Employee"
+		placeholder="Write your Name">
+		
+		<label for="Notice">Your message*</label>
+		<textarea
+			id="Notice"
+			name="Notice"
+			rows="6"
+			placeholder= "Write your message">
+			</textarea>
+			
+			<input name="_redirect" type="hidden" id="name" value="http://localhost/Companytoolbox/contact.php?mail=Jakub">
+			
+		<input value="Send" type="submit">
+		</button>
+	</form>
+	
 	<footer>
 		<div class="footer-frame">
 			<p class="Copyright">© Copyright 2022 Grupa 2 - All Rights Reserved</p>
 		</div>
 	</footer>
-	
-	
-	
+
 <script>
 function myFunction() {
   var x = document.getElementById("myLinks");
